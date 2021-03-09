618505185b8f4011eb2757dbfcd14c33ee6b1d8d
Your Name <you@example.com>
2021-03-09 12:07:09 +0700

    update

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+    define("hostname","localhost");
+    define("username",'user');
+    define("password","ec1L9W4hnfSNcQia");
+    define("dbname","bookstore");
+?>
+<?php
+    $conn=new mysqli(hostname,username,password,dbname);
+    $conn->query("SELECT * FROM `book` WHERE 1");
+    if($conn->connect_error) echo "not connect";
+    else echo "Connect success"
+?>
