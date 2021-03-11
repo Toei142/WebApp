846e03ca9c0d322eb4e46b491658f0d5ac166a2d
BasTz <6306021420205@fitm.kmutnb.ac.th>
2021-03-11 13:14:21 +0700

    update

--- db.php
+++ db.php
@@ -8,5 +8,9 @@
     $conn=new mysqli(hostname,username,password,dbname);
     $conn->query("SELECT * FROM `book` WHERE 1");
     if($conn->connect_error) echo "not connect";
-    else echo "Connect success"
+    else echo "Connect success";
+    
+    class Database{
+
+    }
 ?>
