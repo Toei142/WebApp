e5d48a881f541650172742a57da3dff0f5e74994
Your Name <you@example.com>
2021-03-16 11:28:35 +0700

    update

--- movie.php
+++ movie.php
@@ -12,7 +12,7 @@
     <?php
     $jsonfile = file_get_contents("movies.json");
     ?>
-
+    <select name="year_movie" id="year_m" onchange="sltY()"></select><br>
     <select name="year_movie" id="year_movie" onchange="sltM()"></select><br>
     <input type="text" name="" id="year"><br>
     <input type="text" name="" id="title"><br>
@@ -22,16 +22,24 @@
     <script>
         var str = "";
         var jsonEx = <?php echo $jsonfile ?>;
-        for (var i = 1000; i < 1500; i++) {
-            document.getElementById("year_movie").innerHTML += "<option value='" + jsonEx[i].year + " : " + jsonEx[i].title + "'>" + jsonEx[i].year + " : " + jsonEx[i].title + "</option>";
+        for (var y = 1900; y < 2018; y++) {
+            document.getElementById("year_m").innerHTML += "<option value='" + y + "'>" + y + "</option>";
         }
-        sltM();
+
+        function sltY() {
+            document.getElementById("year_movie").innerHTML = "";
+            for (var i = 0; i < jsonEx.length; i++) {
+                if (jsonEx[i].year == document.getElementById("year_m").value) {
+                    document.getElementById("year_movie").innerHTML += "<option value='" + jsonEx[i].year + " : " + jsonEx[i].title + "'>" + jsonEx[i].year + " : " + jsonEx[i].title + "</option>";
+                }
+            }
+        }
 
         function sltM() {
             var value = document.getElementById("year_movie").value;
             var year = value.substr(0, 5);
             var title = value.substr(7);
-            for (var i = 1000; i < 1500; i++) {
+            for (var i = 0; i < jsonEx.length; i++) {
                 if (year == jsonEx[i].year) {
                     if (title == jsonEx[i].title) {
                         document.getElementById("year").value = jsonEx[i].year;
