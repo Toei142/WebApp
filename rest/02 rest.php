831dff7bb1ac31c7d7bc87427dae3f6926dfcc44
Your Name <you@example.com>
2021-03-18 23:47:41 +0700

    ีupdate

--- rest/02 rest.php
+++ rest/02 rest.php
@@ -7,8 +7,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     echo json_encode(show_data($debug_mode));
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     debug_text("POST METHOD May be implement soon.....", $debug_mode);
-    $message = array("status" => print_r($_POST));
+    //$message = array("status" => print_r($_POST));
     add_data($debug_mode, $_POST['u_name'], $_POST['u_age']);
+    echo json_encode($message);
+} else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    debug_text("DELETE METHOD.....", $debug_mode);
+    delete_id($debug_mode, $_GET['u_id']);
+    $message = array("status" => print_r($_GET['u_id']));
     echo json_encode($message);
 } else {
     debug_text("Error this site Unsupport This request", $debug_mode);
@@ -25,5 +30,11 @@ function add_data($debug_mode, $name, $age)
 {
     $mydb = new db("root", "", "person_data", $debug_mode);
     $mydb->query("INSERT INTO `person`(`name`, `age`) VALUES ('$name',$age)");
+    $mydb->close();
+}
+function delete_id($debug_mode, $id)
+{
+    $mydb = new db("root", "", "person_data", $debug_mode);
+    $mydb->query("DELETE FROM person WHERE id=$id");
     $mydb->close();
 }
