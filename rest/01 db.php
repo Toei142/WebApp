818b0280c526edc3f913e0f5e14d7d332e359d76
Your Name <you@example.com>
2021-03-18 16:12:22 +0700

    update

--- rest/01 db.php
+++ rest/01 db.php
@@ -31,4 +31,4 @@ class db
         $this->db->close();
     }
 }
-
+?>
--- rest/02 rest.php
+++ rest/02 rest.php
@@ -7,6 +7,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     echo json_encode(show_data($debug_mode));
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     debug_text("POST METHOD May be implement soon.....", $debug_mode);
+    $message = array("status" => print_r($_POST));
+    add_data($debug_mode, $_POST['u_name'], $_POST['u_age']);
+    //echo json_encode($message);
 } else {
     debug_text("Error this site Unsupport This request", $debug_mode);
     http_response_code(405);
@@ -17,4 +20,10 @@ function show_data($debug_mode)
     $data = $mydb->query("select * from person");
     $mydb->close();
     return $data;
+}
+function add_data($debug_mode, $name, $age)
+{
+    $mydb = new db("root", "", "person_data", $debug_mode);
+    $mydb->query("INSERT INTO `person`(`name`, `age`) VALUES ({'$name'},{$age})");
+    $mydb->close();
 }
