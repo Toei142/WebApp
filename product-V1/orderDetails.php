162fa401d5119a341fea298b87da907c490767ce
Your Name <you@example.com>
2021-04-05 13:39:19 +0700

    update

--- product-V1/orderDetails.php
+++ product-V1/orderDetails.php
@@ -6,22 +6,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
 
 </head>
 
 <body style="margin: 0;">
-    <div class="navbar">
-        <a href="product_list.php">หน้าแรก</a>
-        <a href="orderDetails.php">ตระกร้าสินค้า</a>
-        <a href="#contact" style="float: right;">ออกจากระบบ</a>
-        <a href="#contact" style="float: right;"> เข้าสู่ระบบ</a>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-body rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ITI SHOP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="product_list.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+                                <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z" />
+                            </svg></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="orderDetails.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            </svg></a>
+                    </li>
 
-    <div class="container" style="margin-top: 100px; width: 30%;">
+                </ul>
+                <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                    <li class="nav-item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                            <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                        </svg>
+                    </li>
 
-        <div class="card">
-            <table>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="container" style="margin-top: 50px; ">
+
+        <div class="shadow p-3 mb-5 bg-body rounded">
+            <table class="table" style="margin: 50px; width: 90%;">
                 <tbody id="body">
                 </tbody>
             </table>
@@ -42,7 +75,7 @@
                     text += "</tr>";
                     for (i = 0; i < arr[0].length; i++) {
                         for (j = 0; j < arr[0][i].length; j++) {
-                            text += "<td>" + arr[0][i][j] + "</td>";
+                            text += "<td scope='col'>" + arr[0][i][j] + "</td>";
                         }
                     }
                     text = "<tr>" + text + "</tr>";
@@ -59,11 +92,11 @@
                         text += "<td>" + arr[1][a].unitPrice + "</td>";
 
                         text += "<td>" + arr[1][a].quantity * arr[1][a].unitPrice + "</td>";
-                        text += "</tr>";
+                        text += "</tr > ";
                         total += parseInt(arr[1][a].quantity * arr[1][a].unitPrice);
                     }
-                    text += '<tr><td>ราคารวม</td><td>' + total + '</td></tr>';
-                    text += '<tr><td><button class="button" style="width:100px" onclick="pay(' + arr[0][0][0] + ')">ชำระเงิน</button></td></tr>';
+                    text += '<tr><td colspan="2">ราคารวม</td><td colspan="2">' + total + '</td></tr>';
+                    text += '<tr><td colspan="4"><button class="btn btn-danger" style="width:100px" onclick="pay(' + arr[0][0][0] + ')">ชำระเงิน</button></td></tr>';
                     document.getElementById("body").innerHTML = text;
                 }
             }
@@ -76,8 +109,9 @@
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     if (this.responseText == 1) {
-                        alert("เพิ่มสำเร็จ");
-                    } else alert("เพิ่มสินค้าไม่สำเร็จ");
+                        alert("ชำระเงิน");
+                        showOrder();
+                    } else alert("ชำระเงินไม่สำเร็จ");
                 }
             }
             xhttp.open("POST", "rest.php", true);
--- product-V1/product_list.php
+++ product-V1/product_list.php
@@ -6,30 +6,104 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    <style>
+        /* button */
+
+        .button2 {
+            display: inline-block;
+            border-radius: 4px;
+            background-color: rgb(243, 103, 103);
+            border: none;
+            color: #FFFFFF;
+            text-align: center;
+            font-size: 15px;
+            padding: 10px;
+            width: 90%;
+            transition: all 0.5s;
+            cursor: pointer;
+            margin: 5px;
+        }
+
+        .button2 span {
+            cursor: pointer;
+            display: inline-block;
+            position: relative;
+            transition: 0.5s;
+        }
+
+        .button2 span:after {
+            content: '\00bb';
+            position: absolute;
+            opacity: 0;
+            top: 0;
+            right: -20px;
+            transition: 0.5s;
+        }
+
+        .button2:hover span {
+            padding-right: 25px;
+        }
+
+        .button2:hover span:after {
+            opacity: 1;
+            right: 0;
+        }
+
+        .button2:hover {
+            background-color: red;
+        }
+    </style>
 
 </head>
 
 <body onload="productList()" style="margin: 0;">
-    <div class="navbar">
-        <a href="product_list.php">หน้าแรก</a>
-        <a href="orderDetails.php">ตระกร้าสินค้า</a>
-        <a href="#contact" style="float: right;">ออกจากระบบ</a>
-        <a href="#contact" style="float: right;"> เข้าสู่ระบบ</a>
-    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-body rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ITI SHOP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="product_list.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+                                <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z" />
+                            </svg></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="orderDetails.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            </svg></a>
+                    </li>
+
+                </ul>
+                <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                    <li class="nav-item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                            <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                        </svg>
+                    </li>
 
 
-    <div class="container" style="margin-top: 70px;">
+                </ul>
 
-        <div>
-            <table style="background-color:;">
-                <tbody id="body">
-                </tbody>
-            </table>
+            </div>
         </div>
-        <div id="product"></div>
+    </nav>
+
+    <div class="container" style="width: 70%;">
+        <div class="row" id="product" style="text-align: center;">
+        </div>
+
     </div>
-    <img src=" " height="" alt="">
+
     <script>
         let data;
 
@@ -39,18 +113,29 @@
                 text = "";
                 if (this.readyState == 4 && this.status == 200) {
                     data = JSON.parse(this.responseText);
-                    text += "<div class='row'>";
+
                     for (i = 0; i < data.length; i++) {
-                        text += "<div class='column'>";
-                        text += "<div class='card'>";
-                        text += "<img src='" + data[i].image + "' width='5%' height='5%'><br>";
-                        text += data[i].name + "<br>";
-                        text += "฿ " + data[i].cost + " <input type='number' name='' id='n" + i + "' size='4' max='" + data[i].stock + "' min='1' value='1'>";
-                        text += " <button class='button2' style='vertical-align:middle' onclick='addProduct(" + i + ")'><span>เพิ่มไปยังรถเข็น</span></button>";
+                        text += "<div class='col-3' style='padding:10px'>";
+                        text += '<div class="card shadow p-3 mb-5 bg-body rounded" style="width: 15rem;border:none; ">';
+                        text += '<img src="' + data[i].image + '" class="card-img-top rounded mx-auto d-block" style="width: 100px;">';
+                        text += '<div class="card-body">';
+                        text += '<h5 class="card-title">' + data[i].name + '</h5>';
+                        text += "       ราคา ฿ " + data[i].cost;
+                        text += " <input type='number' class='form-control' name='' id='n" + i + "' size='4' max='" + data[i].stock + "' min='1' value='1'>";
+                        text += "       <button class='button2' style='vertical-align:middle' onclick='addProduct(" + i + ")'><span>เพิ่มไปยังรถเข็น</span></button>";
+                        text += "   </div>";
+                        text += "</div>";
                         text += "</div>";
                         text += "</div>";
                     }
-                    text += "</div>";
+
+
+
+
+
+
+
+
                     document.getElementById("product").innerHTML = text;
 
                 }
